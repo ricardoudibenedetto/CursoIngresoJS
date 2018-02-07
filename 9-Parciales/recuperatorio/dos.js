49fc37dfbb92f629478622742200dfc9cb2d45a7
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-02-06 22:20:10 -0300

    ejercicios resueltos
    superficie de un cuadrado y importe con descuento

--- 9-Parciales/recuperatorio/dos.js
+++ 9-Parciales/recuperatorio/dos.js
@@ -1,4 +1,13 @@
 function Mostrar()
 {
-  
+  var importeproducto;
+  var importedescuento;
+  var importefinal;
+
+  importeproducto=prompt("ingrese importe del producto");
+
+  importedescuento=importeproducto*0.25;
+  importefinal=importeproducto-importedescuento;
+
+  alert("Importe por fin de temporada: "+importefinal);
 }
--- 9-Parciales/recuperatorio/uno.js
+++ 9-Parciales/recuperatorio/uno.js
@@ -1,5 +1,12 @@
 
 function Mostrar()
 {
+var base;
+var superficie;
 
+base=document.getElementById('laBase').value;
+
+superficie=base*2;
+
+alert("La superficie del cuadrado es : "+superficie);
 }
