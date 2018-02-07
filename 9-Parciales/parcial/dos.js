0a79c18a18ccd6065abe078d138eb8355df566a9
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-02-06 22:28:48 -0300

    ejercicio 2
    sumar 21% de iva con parseIn en variables

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,14 @@
 function Mostrar()
 {
-  
+  var importe;
+  var importefinal;
+  var importeiva;
+
+  importe=prompt("Imtrodusca el inporte de un producto");
+
+ importeiva=importe*0.21;
+
+ importefinal=parseInt(importe)+parseInt(importeiva);
+
+ document.getElementById('importeFinal').value=importefinal;
 }
