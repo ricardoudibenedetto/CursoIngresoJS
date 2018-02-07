2d83e077e90508ff5686577521994604d81f6df0
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-02-07 10:19:46 -0300

    ejercicios resueltos con parseInt

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -4,5 +4,18 @@ Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
 
+	var numerUno;	
+	var numeroDos;
+	var resultado;
+
+	numerUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+
+	numerUno=parseInt(numerUno);
+	numeroDos=parseInt(numeroDos);
+
+	resultado=numerUno+numeroDos;
+	alert("La suma es: "+resultado);
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo;
+	var sueldoAumento;
+	var resultado;
 
+	sueldo=document.getElementById('sueldo').value;
+
+	sueldo=parseInt(sueldo);
+	sueldoAumento=sueldo*0.1;
+	resultado=sueldo+sueldoAumento;
+	
+	document.getElementById('resultado').value=resultado;
 	
 }
