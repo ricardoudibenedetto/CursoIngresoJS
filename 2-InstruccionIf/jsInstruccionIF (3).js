15956f3b5ec99de98deedef9243fe3bddad783cb
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-02-07 11:48:12 -0300

    if y else

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,27 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
 
+	edad=document.getElementById('edad').value;
+	if (edad>17) 
+	{
+		alert("Usted es mayor de edad!");
+	}
+	else
+	 {
+	 	alert("Usted es menor de edad.");
+	 }
+
+	/*edad=document.getElementById('edad').value;
+	if (edad>17) 
+	{
+		alert("Usted es mayor de edad!");
+	}
+	if (edad<18)
+	 {
+	 	alert("Usted es menor de edad.");
+	 }
+	 */
 
 }//FIN DE LA FUNCIÓN
