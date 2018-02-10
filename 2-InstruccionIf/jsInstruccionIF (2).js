f21e7cfb982c0c5e046fb00aa0e28136babfb574
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-02-10 20:07:01 -0300

    ejercicios resueltos
    solo if

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,9 +2,9 @@ function Mostrar()
 {
 //tomo la edad  
 	var edad;
-
+//guardo en la variable
 	edad=document.getElementById('edad').value;
-
+//si edad es mayor a 17 muestra la alerta
 	if (edad>17)
 	 {
 	 	alert("Usted es mayor de edad");
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad=document.getElementById('edad').value;
+
+if(edad>12&&edad<18)
+{
+	alert("Usted es adolecento")
+}
 
 
 }//FIN DE LA FUNCIÓN
