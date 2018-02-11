d6eedb98296f4d4163649079a0b9a0343eac4539
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-02-11 13:37:38 -0300

    resueltos

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,29 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+
+edad=document.getElementById('edad').value;
+//si la persona es mayor a 17 es mayor de edad
+if(edad>17)
+{
+	alert("Usted es mayor de edad");
+
+}
+//sino es:
+else
+{
+//si es mayor de 12 y menor a 18 es adolecente
+	if(edad<18&&edad>12)
+	{
+		alert("usted es adolecente");
+	}
+//sino es los que falta
+	else
+	{
+		alert("usted es un niño");
+	}
+	}
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,17 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+var estadoCivil;
 
-	
+edad=document.getElementById('edad').value;
+
+estadoCivil=document.getElementById('estadoCivil').value;
+
+if(edad<18&&estadoCivil!="soltero")
+{
+alert("Es muy pequeño para No ser soleto");
+}
 
 
 }//FIN DE LA FUNCIÓN
