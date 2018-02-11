68dbb289f2e5350fe051428ea9f1c36ab5898a88
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-02-11 13:38:33 -0300

    Esta mal

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,20 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+var estadoCivil;
 
-	
-
+edad=document.getElementById('edad').value;
+estadoCivil=document.getElementById('estadoCivil').value;
+//esta mal
+/*if(edad>18&&estadoCivil=="soltero")
+{
+ alert("Es soltero y no es menor");
+}
+else
+{
+	alert("Es soltero y no es menor");
+}	
+*/
 
 }//FIN DE LA FUNCIÓN
