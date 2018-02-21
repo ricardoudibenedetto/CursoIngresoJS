269e53c78f8411577a2395a83d5cd257d5480744
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-02-21 10:50:03 -0300

    for variantes

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,54 @@
 function Mostrar()
 {
+	var contador=-10;
+	var iteraciones=10;
+	var par=0;
+	var impar=0;
+	var positivo=0;
+	var negativo=0;
+	var contadorDeCero
+
+	/*for (var i = 0; i<10; i++) {
+			}*/
+
+	for(contador=-10;contador<iteraciones;contador++)
+	{
+		document.write("<br>"+contador);
+		if (contador==0)
+		 {
+		 	contadorDeCero++;
+		 }else
+		 {
+
+
+			if (contador%2==0)
+			 {
+			 	par++;
+			}
+			 else
+			 {
+			 	impar++;
+			 }
+			  if (contador>0)
+			  {
+			  	positivo++;
+			  }else 
+			  {
+			  	negativo++;
+			  }
+		  }
+		
+
+		
+		
+
+		
+
+	}
+		document.write("<br>"+"impar:"+impar);//SALE EN 10 PORQUE EL CONTADOR LO SUMA ANTES DE COMPARAR.
+		document.write("<br>"+"par:"+par);
+		document.write("<br>"+"positivo:"+positivo);
+		document.write("<br>"+"negativo:"+negativo);
+
 
 }
