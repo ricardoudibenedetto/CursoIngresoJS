991709bfd9bc50e608706821d66c04a40ce0e6e3
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-02-20 21:23:05 -0300

    Iluminacion "terminado"

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -9,6 +9,101 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
 
  */
 function CalcularPrecio () 
-{
- 	
-}
+	{
+	  var precioLamparas;
+	  var cantidadDeLamparas;
+	  var descuentoEnPorcentaje;
+	  var marca;
+	  var precioBruto;
+	  var precioFinal;
+	  var precioIbb;
+
+	  precioLamparas=35;
+	  descuentoEnPorcentaje=1;
+	  precioIbb=1.1;
+	  
+
+	  cantidadDeLamparas=document.getElementById('Cantidad').value;
+	  marca=document.getElementById('Marca').value;
+	  precioBruto=cantidadDeLamparas*precioLamparas;
+
+	  console.info(cantidadDeLamparas); //informa en la consola la cantidad de lamparas seleccionadas
+	  cantidadDeLamparas=parseInt(cantidadDeLamparas); //paso el valor del input a numerico
+
+
+	  if (cantidadDeLamparas>5)
+	   {
+	   	descuentoEnPorcentaje=0.5;
+	   } //LLAVE PRIMER IF
+	   else
+	   {
+	   	if (cantidadDeLamparas==5)
+	   	{
+	   		if (marca=="ArgentinaLuz")
+	   		 {
+	   		 	descuentoEnPorcentaje=0.6;
+	   		 }
+	   		 else
+	   		 {
+	   		 	descuentoEnPorcentaje=0.7;
+	   		 }
+
+	   	}//llave segundo if
+	   	else
+	   	{
+	   		if (cantidadDeLamparas==4)
+	   		 {
+	   		 	if(marca=="ArgentinaLuz"||marca=="FelipeLamparas")
+	   		 	{
+	   		 		descuentoEnPorcentaje=0.75;
+	   		 	}
+	   		 	else
+	   		 	{
+	   		 		descuentoEnPorcentaje=0.8;
+	   		 	}
+
+	   		 }
+	   		 else
+	   		 {
+	   		 	if (cantidadDeLamparas==3) 
+	   		 	{
+	   		 		if (marca=="ArgentinaLuz")
+	   		 		 {
+	   		 		 	descuentoEnPorcentaje=0.85;
+	   		 		 }
+	   		 		 else
+	   		 		 {
+	   		 		 	if (marca=="FelipeLamparas")
+	   		 		 	 {
+	   		 		 	 	descuentoEnPorcentaje=0.9;
+	   		 		 	 }
+	   		 		 	 else
+	   		 		 	 {
+	   		 		 	 	descuentoEnPorcentaje=0.95;
+
+	   		 		 	 }
+
+	   		 		 }
+	   		 	}
+
+	   		 }
+	   	}//llave segundo else
+
+
+
+
+	   }//LLAVE DE PRIMER ELSE
+
+	   precioFinal=precioBruto*descuentoEnPorcentaje;
+	   if (precioFinal<120) 
+	   {
+	   	document.getElementById('precioDescuento').value=precioFinal;
+	   }
+	   else{
+	   	precioFinal=precioFinal*precioIbb;
+	   	 document.getElementById('precioDescuento').value=precioFinal;
+
+	   }
+	   
+
+	}
