ae90139e51a0930a0e616bed4c24d18044d091c5
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-02-24 12:10:57 -0300

    Add files via upload

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,7 +1,9 @@
 /*ingreso de alumnos 
 , pedir nombre,
  cuantos alumnos ingrese
- ,final document.write ingrese 9 alumnos*/
+ ,final document.write ingrese 9 alumnos
+ cantidad de varones desaprobados,
+ nombre de mejor nota*/
 function Mostrar()
 {
 	var nombreAlumnos;
@@ -12,42 +14,73 @@ function Mostrar()
 	var sexo;
 	var sexoM=0;
 	var sexoF=0;
+	var desaprobadosM=0;
+	var maximo=0;
+	var nombreMejorNota=""; //Primer nombre, El maximo
+
+
+
 
 	while(continuar!="no")
 	{
 		nombreAlumnos=prompt("ingrese nombre de alumno: ");
 		cantidadDeAlumnos=cantidadDeAlumnos+1;
+		//NOTAS
 		nota=prompt("ingrese un nota");		
  		nota=parseInt(nota);
  		while(nota<0 ||nota>10)
  		{
  			nota=prompt("ingrese un nota");		
  			nota=parseInt(nota);
-
  		}
+ 		//CANTIDAD DE SEXO Y SUMA 
  		sexo=prompt("ingrese sexo f o m");
- 		while(sexo!="f" || sexo!="m")
+ 		while(sexo!="f" && sexo!="m")
  		{
  			sexo=prompt("ingrese sexo");		
  		}
- 		if (sexo="f") 
+ 		if (sexo=="f") 
  		{
- 			sexoF+1;
+ 			sexoF++;
  		}
- 		else{
- 			sexoM+1;
+ 		else
+ 		{
+ 			sexoM++;
  		}
+
+ 		//CANTIDAD DE VARONES desaprobados
+ 		if(sexoM&&nota<4)
+ 		 {
+ 		 	desaprobadosM++;
+ 		 }
+ 		 //Nombre de la mejor nota "el primero ", sacar maximo!!
+ 		 if(cantidadDeAlumnos==1)
+ 		 {
+ 		 	maximo=nota;
+ 		 	nombreMejorNota=nombreAlumnos;
+ 		 }
+ 		 else
+ 		 {
+ 		 	if (nota>maximo)
+ 		 	 {
+ 		 	 	maximo=nota;
+ 		 	 	nombreMejorNota=nombreAlumnos;
+ 		 	 }
+
+ 		 }
+ 		 
 
  		acumulador=acumulador+nota;
-
-		
-		continuar=prompt("No para salir ");
+ 		continuar=prompt("No para salir ");
 	}
 	promedio=acumulador/cantidadDeAlumnos;
 
 	document.write("La cantidad de alumnos ingresados son: "+cantidadDeAlumnos);
-	document.write("el promedio de las notas es: "+promedio);
-	document.write("La cantidad de sexo masculino es: "+sexoM);
-	document.write("La cantidad de sexo femenino es: "+sexoF);
+	document.write("<br>"+"el promedio de las notas es: "+promedio);
+	document.write("<br>"+"La cantidad de sexo masculino es: "+sexoM);
+	document.write("<br>"+"La cantidad de sexo femenino es: "+sexoF);
+	document.write("<br>"+"La cantidad de sexo masculino desaprobados es: "+desaprobadosM);
+	document.write("<br>"+"El nombre de la mejor nota es: "+nombreMejorNota);
+
 
 }
